b720c4c0bdd4f9ed353987c9e1967e5c2af2aca6
Kwon Juhee <joo970905@naver.com>
2018-10-13 17:25:07 +0900

    2018.10.13
    자바 첫 수업!

--- src/java_practice/ex2_10.java
+++ src/java_practice/ex2_10.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class ex2_10 {
 	public static void main(String[] args) {
+		// 입력값 받기
 		Scanner scanner = new Scanner(System.in);
 
+		// 출력
 		System.out.print("점수를 입력하시오: ");
-		int score = scanner.nextInt();
-		if (score >= 80)
-			System.out.println("축하합니다! 합격입니다.");
 
-		scanner.close();
+		// 입력받은 값을 score에 담기
+		int score = scanner.nextInt();
+
+		// if 조건문
+		if (score >= 80)
+			System.out.println("축하합니다! 합격입니다."); // 출력
+
+		scanner.close(); // 원래 안적어도 됌
 
 		// TODO Auto-generated method stub
 
